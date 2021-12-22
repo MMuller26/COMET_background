288804dcd7e7fb08fcbf78192519c65b2d9d7eee
Marjolein Muller <M.Muller-3@student.tudelft.nl>
2021-12-22 10:51:19 +0100

    with RMSE bar graph added

--- Matlab_Background_Combined_Lifetime_Plots.m
+++ Matlab_Background_Combined_Lifetime_Plots.m
@@ -5,21 +5,26 @@ clc
 
 %% mid-air backgrounds
 load('mid_air_10_background.mat')
+load('lifetime_in.mat')
+
 mid_air_10 = lifetime_mid_air_new;
+RMSE_mid_air_10 = sqrt(mean((mid_air_10-lifetime_in).^2));
 
 load('mid_air_20_background.mat')
 mid_air_20 = lifetime_mid_air_new;
+RMSE_mid_air_20 = sqrt(mean((mid_air_20-lifetime_in).^2));
 
 load('mid_air_30_background.mat')
 mid_air_30 = lifetime_mid_air_new;
+RMSE_mid_air_30 = sqrt(mean((mid_air_30-lifetime_in).^2));
 
 load('mid_air_40_background.mat')
 mid_air_40 = lifetime_mid_air_new;
+RMSE_mid_air_40 = sqrt(mean((mid_air_40-lifetime_in).^2));
 
 load('mid_air_50_background.mat')
 mid_air_50 = lifetime_mid_air_new;
-
-load('lifetime_in.mat')
+RMSE_mid_air_50 = sqrt(mean((mid_air_50-lifetime_in).^2));
 
 figure(1)
 plot(lifetime_in, mid_air_10, '-o', lifetime_in, mid_air_20, '-o', lifetime_in, mid_air_30, '-o', lifetime_in, mid_air_40, '-o', lifetime_in, mid_air_50, '-o')
@@ -33,18 +38,23 @@ title('new vs. input lifetime for 10-50% background COMET mid-air')
 %% skin backgrounds
 load('skin_10_background.mat')
 skin_10 = lifetime_skin_2Hz_new;
+RMSE_skin_10 = sqrt(mean((skin_10-lifetime_in).^2));
 
 load('skin_20_background.mat')
 skin_20 = lifetime_skin_2Hz_new;
+RMSE_skin_20 = sqrt(mean((skin_20-lifetime_in).^2));
 
 load('skin_30_background.mat')
 skin_30 = lifetime_skin_2Hz_new;
+RMSE_skin_30 = sqrt(mean((skin_30-lifetime_in).^2));
 
 load('skin_40_background.mat')
 skin_40 = lifetime_skin_2Hz_new;
+RMSE_skin_40 = sqrt(mean((skin_40-lifetime_in).^2));
 
 load('skin_50_background.mat')
 skin_50 = lifetime_skin_2Hz_new;
+RMSE_skin_50 = sqrt(mean((skin_50-lifetime_in).^2));
 
 figure(2)
 loglog(lifetime_in, skin_10, '-o', lifetime_in, skin_20, '-o', lifetime_in, skin_30, '-o', lifetime_in, skin_40, '-o', lifetime_in, skin_50, '-o')
@@ -58,18 +68,23 @@ title('new vs. input lifetime for 10-50% background COMET skin')
 %% old laser backgrounds
 load('OL_10_background.mat')
 OL_10 = lifetime_OL_new;
+RMSE_OL_10 = sqrt(mean((OL_10-lifetime_in).^2));
 
 load('OL_20_background.mat')
 OL_20 = lifetime_OL_new;
+RMSE_OL_20 = sqrt(mean((OL_20-lifetime_in).^2));
 
 load('OL_30_background.mat')
 OL_30 = lifetime_OL_new;
+RMSE_OL_30 = sqrt(mean((OL_30-lifetime_in).^2));
 
 load('OL_40_background.mat')
 OL_40 = lifetime_OL_new;
+RMSE_OL_40 = sqrt(mean((OL_40-lifetime_in).^2));
 
 load('OL_50_background.mat')
 OL_50 = lifetime_OL_new;
+RMSE_OL_50 = sqrt(mean((OL_50-lifetime_in).^2));
 
 figure(3)
 plot(lifetime_in, OL_10, '-o', lifetime_in, OL_20, '-o', lifetime_in, OL_30, '-o', lifetime_in, OL_40, '-o', lifetime_in, OL_50, '-o')
@@ -99,3 +114,15 @@ ylabel('new lifetime')
 xlim([0 200])
 ylim([0 200])
 title('new vs. input lifetime for 3 backgrounds of 50%')
+
+%% Barchart of RMSE of different set-ups
+
+background = categorical({'10%'; '20%'; '30%'; '40%'; '50%'});
+bars = [RMSE_mid_air_10 RMSE_skin_10 RMSE_OL_10; RMSE_mid_air_20 RMSE_skin_20 RMSE_OL_20; RMSE_mid_air_30 RMSE_skin_30 RMSE_OL_30; RMSE_mid_air_40 RMSE_skin_40 RMSE_OL_40; RMSE_mid_air_50 RMSE_skin_50 RMSE_OL_50];
+
+figure(6)
+bar(background, bars)
+legend('mid-air', 'skin', 'old laser')
+xlabel('percentage of background signal')
+ylabel('RMSE predicted vs. input lifetime')
+title('RMSE of predicted vs. input lifetime for different setups and percentages of background signal')
